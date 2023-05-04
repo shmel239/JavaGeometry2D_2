e8c955616c889b1186e8d4ebdcd4c3943df2563d
shibaevdd.24 <daniilsh_2005@mail.ru>
2023-05-04 10:28:51 +0300

    adding points works

--- src/main/java/panels/Panel.java
+++ src/main/java/panels/Panel.java
@@ -1,10 +1,12 @@
 package panels;
 
 import io.github.humbleui.jwm.Event;
+import io.github.humbleui.jwm.EventMouseMove;
 import io.github.humbleui.jwm.Window;
 import io.github.humbleui.skija.Canvas;
 import io.github.humbleui.skija.Paint;
 import misc.CoordinateSystem2i;
+import misc.Vector2i;
 
 import java.util.function.Consumer;
 
@@ -15,6 +17,18 @@ import static app.Application.C_RAD_IN_PX;
  * Класс панели
  */
 public abstract class Panel implements Consumer<Event> {
+    /**
+     * последнее движение мыши
+     */
+    protected Vector2i lastMove = new Vector2i(0, 0);
+    /**
+     * было ли оно внутри панели
+     */
+    protected boolean lastInside = false;
+    /**
+     * последняя СК окна
+     */
+    protected CoordinateSystem2i lastWindowCS;
     /**
      * отступ в пикселях
      */
@@ -72,7 +86,10 @@ public abstract class Panel implements Consumer<Event> {
         paintImpl(canvas, windowCS);
         // восстанавливаем область рисования
         canvas.restore();
+        // сохраняем СК окна
+        lastWindowCS = windowCS;
     }
+
 
     /**
      * Метод рисованияв конкретной реализации
@@ -81,6 +98,31 @@ public abstract class Panel implements Consumer<Event> {
      * @param windowCS СК окна
      */
     public abstract void paintImpl(Canvas canvas, CoordinateSystem2i windowCS);
-
+    /**
+     * Проверка, содержит ли панель координаты
+     *
+     * @param pos положение
+     * @return флаг, содержит или нет
+     */
+    public boolean contains(Vector2i pos) {
+        if (lastWindowCS != null)
+            return lastWindowCS.checkCoords(pos);
+        return false;
+    }
+    /**
+     * Обработчик событий
+     * при перегрузке обязателен вызов реализации предка
+     *
+     * @param e событие
+     */
+    @Override
+    public void accept(Event e) {
+        if (e instanceof EventMouseMove ee) {
+            // сохраняем последнее положение мыши
+            lastMove = new Vector2i(ee);
+            // сохраняем флаг, был ли курсор внутри панели
+            lastInside = contains(lastMove);
+        }
+    }
 }
 
